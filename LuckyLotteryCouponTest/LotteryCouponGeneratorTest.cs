f5d1305c4679eb440602ee64c6afac51a580a8d6
Carsten Rickers <carsten.rickers@mail.dk>
2021-03-12 21:33:41 +0100

    Add some unit tests

---
+++ LuckyLotteryCouponTest/LotteryCouponGeneratorTest.cs
@@ -0,0 +1,43 @@
+﻿using LuckyLotteryCouponApp;
+using NSubstitute;
+using Xunit;
+
+namespace LuckyLotteryCouponTest
+{
+    public class LotteryCouponGeneratorTest
+    {
+        [Fact]
+        public void TestGenerateSimpleCoupon()
+        {
+            // Arrange:
+            int expectedValue = 1;
+            var randomGenerator = CreateRandomNumberGeneratorMock(expectedValue);
+            var lotteryCouponGenerator = new LotteryCouponGenerator(10, 7, randomGenerator);
+
+            // Act:
+            var lotteryCoupon = lotteryCouponGenerator.CreateEmptyLotteryCoupon();
+            lotteryCouponGenerator.RefillLotteryCoupon(lotteryCoupon);
+
+            // Assert:
+            CheckAllRowsAndColumnValues(lotteryCoupon, expectedValue);
+        }
+
+        private static void CheckAllRowsAndColumnValues(LotteryCoupon lotteryCoupon, int expectedValue)
+        {
+            foreach (var row in lotteryCoupon.Rows)
+            {
+                foreach (var n in row)
+                {
+                    Assert.Equal(expectedValue, n);
+                }
+            }
+        }
+
+        private static IRandomNumberGenerator CreateRandomNumberGeneratorMock(int value)
+        {
+            var randomGenerator = Substitute.For<IRandomNumberGenerator>();
+            randomGenerator.Next().Returns(value);
+            return randomGenerator;
+        }
+    }
+}
---
+++ LuckyLotteryCouponTest/LuckyLotteryCouponGeneratorTest.cs
@@ -0,0 +1,57 @@
+﻿using LuckyLotteryCouponApp;
+using NSubstitute;
+using Xunit;
+
+namespace LuckyLotteryCouponTest
+{
+    public class LuckyLotteryCouponGeneratorTest
+    {
+        [Fact]
+        public void TestLuckyLotteryCouponGeneratorWithOneThread()
+        {
+            // Arrange:
+            var expectedLotteryCoupon = new LotteryCoupon(10, 7);
+            var lotteryCouponGenerator = CreateLotteryCouponGeneratorMock(expectedLotteryCoupon);
+            var lotteryCouponChecker = CreateLotteryCouponCheckerMock();
+            var luckyLotteryCouponGenerator = new LuckyLotteryCouponGenerator(lotteryCouponGenerator, lotteryCouponChecker);
+
+            // Act:
+            (var result, long numberOfAttempts) = luckyLotteryCouponGenerator.Generate(1);
+
+            // Assert:
+            Assert.Same(expectedLotteryCoupon, result);
+            Assert.Equal(1, numberOfAttempts);
+        }
+
+        [Fact]
+        public void TestLuckyLotteryCouponGeneratorWithFourThreads()
+        {
+            // Arrange:
+            var expectedLotteryCoupon = new LotteryCoupon(10, 7);
+            var lotteryCouponGenerator = CreateLotteryCouponGeneratorMock(expectedLotteryCoupon);
+            var lotteryCouponChecker = CreateLotteryCouponCheckerMock();
+            var luckyLotteryCouponGenerator = new LuckyLotteryCouponGenerator(lotteryCouponGenerator, lotteryCouponChecker);
+
+            // Act:
+            (var result, long numberOfAttempts) = luckyLotteryCouponGenerator.Generate(4);
+
+            // Assert:
+            Assert.Same(expectedLotteryCoupon, result);
+            Assert.Equal(4, numberOfAttempts);
+        }
+
+        private ILotteryCouponGenerator CreateLotteryCouponGeneratorMock(LotteryCoupon expectedLotteryCoupon)
+        {
+            var lotteryCouponGenerator = Substitute.For<ILotteryCouponGenerator>();
+            lotteryCouponGenerator.CreateEmptyLotteryCoupon().Returns(expectedLotteryCoupon);
+            return lotteryCouponGenerator;
+        }
+
+        private ILotteryCouponChecker CreateLotteryCouponCheckerMock()
+        {
+            var lotteryCouponChecker = Substitute.For<ILotteryCouponChecker>();
+            lotteryCouponChecker.Check(Arg.Any<LotteryCoupon>()).Returns(true);
+            return lotteryCouponChecker;
+        }
+    }
+}
